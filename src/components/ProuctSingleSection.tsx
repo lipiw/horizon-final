d0a444e3bf92eb7180e1df17f992f9a803ae0f6a
Felipe Gonçalves <47393970+lipiw@users.noreply.github.com>
2023-11-23 18:35:04 -0300

    Update ProuctSingleSection.tsx

--- src/components/ProuctSingleSection.tsx
+++ src/components/ProuctSingleSection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
-import Link  from "next/link";
+import Link from "next/link";
 
 export function ProductSingleSection() {
   const router = useRouter();
@@ -13,9 +13,9 @@ export function ProductSingleSection() {
   const description = router.query.description;
 
   // Verifica se 'photo' é uma string e não está vazia
-  const photoUrl = typeof photo === 'string' && photo !== '' ? photo : '/placeholder.jpg';
-  const idUrl = typeof id === 'string' && id !== '' ? id : '/placeholder.jpg';
-
+  const photoUrl =
+    typeof photo === "string" && photo !== "" ? photo : "/placeholder.jpg";
+  const idUrl = typeof id === "string" && id !== "" ? id : "/placeholder.jpg";
 
   return (
     <div className="mt-10 flex flex-col justify-between lg:flex-row gap-16 lg:items-center">
@@ -31,26 +31,29 @@ export function ProductSingleSection() {
           <span className=" text-violet-600 font-semibold">{venue}</span>
           <h1 className="text-3xl font-bold mt-4">{title}</h1>
         </div>
-        {description ? (
-          <p className="text-gray-700">{description}</p>
-        ) : (
-          <p className="text-gray-700">
-            Prepare-se para um evento inesquecível! Este evento promete ser uma
-            experiência única, repleta de momentos emocionantes e oportunidades
-            de aprendizado. Seja você um entusiasta, um profissional ou
-            simplesmente alguém em busca de uma experiência nova e emocionante,
-            este evento tem algo para todos. Venha e junte-se a nós para uma
-            celebração de ideias, inspirações e conexões interpessoais. Não
-            perca a chance de fazer parte desta experiência incrível!
-          </p>
-        )}
+        <p className="text-gray-700">
+          Prepare-se para um evento inesquecível! Este evento promete ser uma
+          experiência única, repleta de momentos emocionantes e oportunidades de
+          aprendizado. Seja você um entusiasta, um profissional ou simplesmente
+          alguém em busca de uma experiência nova e emocionante, este evento tem
+          algo para todos. Venha e junte-se a nós para uma celebração de ideias,
+          inspirações e conexões interpessoais. Não perca a chance de fazer
+          parte desta experiência incrível!
+        </p>
         <h6 className="text-2xl font-semibold mt-5">{datetime}</h6>
-        <a href={idUrl} target="_blank" className="mt-10 bg-violet-800 text-white font-semibold text-center py-3 px-16 rounded-xl h-full">
+        <a
+          href={idUrl}
+          target="_blank"
+          className="mt-10 bg-violet-800 text-white font-semibold text-center py-3 px-16 rounded-xl h-full"
+        >
           Acessar evento
         </a>
 
-        <Link href="/products" className="border text-black font-semibold text-center py-3 px-16 rounded-xl h-full">
-            Retornar para lista de eventos
+        <Link
+          href="/products"
+          className="border text-black font-semibold text-center py-3 px-16 rounded-xl h-full"
+        >
+          Retornar para lista de eventos
         </Link>
       </div>
     </div>
